3d288efdd5198c07c2d86d318664c21760d87880
release_the_kraken <wmichal13@wp.pl>
2022-07-03 11:41:22 +0200

    initial commit

---
+++ jdbc_hibernate/src/main/java/jdbc/DatabaseOperations.java
@@ -0,0 +1,24 @@
+package jdbc;
+
+import java.sql.*;
+
+import static jdbc.Constants.*;
+
+public class DatabaseOperations {
+    public static void main(String[] args) {
+        try ( Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);){
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM animal");
+
+            while(resultSet.next()){
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                System.out.println("id: %s, name: %s, age: %s".formatted(id, name, age));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
